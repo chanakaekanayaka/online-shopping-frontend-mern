284e9081d86e724ee390949dc3ade1f2108938c3
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-12-22 11:53:37 +0530

    categories updated✏️

--- src/pages/client/productPage.jsx
+++ src/pages/client/productPage.jsx
@@ -10,7 +10,7 @@ export default function ProductPage() {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   //  Categories 
-  const categories = ["Cricket", "Football", "Tennis", "Rugby", "Beauty", "Accessories"];
+  const categories = ["Cricket", "Football", "Tennis", "Rugby", "Footwear", "Fitness"];
 
   useEffect(() => {
     if (isLoading) {
